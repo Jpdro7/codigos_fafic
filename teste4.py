ccbd30c34de8f30d854cabd1e312f10e89d870ea
Jpdro7 <almeidanunesjoaopedro2@gmail.com>
2025-02-19 12:56:14 -0300

    Delete teste4.py

--- teste4.py
+++
@@ -1,8 +0,0 @@
-n1 = int(input('Digite o primeiro número a ser calculado'))
-n2 = int(input('Digite o segundo número a ser calculado'))
-n3 = int(input('Digite o terceiro número a ser calculado'))
-
-soma = n1 + n2 + n3
-media = soma / 3
-
-print(f"Sua media foi {media}")
