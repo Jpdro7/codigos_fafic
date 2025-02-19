0a6b980d6d062f796185e01998d2a659313aa701
Jpdro7 <almeidanunesjoaopedro2@gmail.com>
2025-02-19 12:57:16 -0300

    Delete teste9.py

--- teste9.py
+++
@@ -1,6 +0,0 @@
-valor = float(input("Digite quanto voce ganha por hora: "))
-horas = float(input("Digite o numero de horas trabalhadas no mes: "))
-
-salario = valor * horas
-
-print(f"O seu salario no mes sera de R$: {salario:.2f}")
