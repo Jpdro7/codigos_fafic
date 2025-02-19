fbfecd2b9de9170b21464678473de1f79495d75b
Jpdro7 <almeidanunesjoaopedro2@gmail.com>
2025-02-19 12:46:04 -0300

    Add files via upload

---
+++ teste1.py
@@ -0,0 +1,10 @@
+nome = "João Pedro"
+sobrenome = "Almeida Nunes"
+idade = "17 Anos"
+print(f"Bem Vindo Magnata {nome} {sobrenome} {idade}")
+
+num = 7
+num2 = 25
+
+subtração = num - num2
+print(f"O resultado da Subtração é = {subtração}")
---
+++ teste10.py
@@ -0,0 +1,6 @@
+fahrenheit = float(input("Digite a temperatura em graus fahrenheit: "))
+celsius = (fahrenheit - 32) * 5 / 9
+
+print(f"A temperatura em graus Celsius e: {celsius:.2f}")
+
+
