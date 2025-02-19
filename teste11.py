50640cd40eab433e9e8d3801c46606fb3ad701a4
Jpdro7 <almeidanunesjoaopedro2@gmail.com>
2025-02-19 12:54:06 -0300

    Delete teste11.py

--- teste11.py
+++
@@ -1,8 +0,0 @@
-def grauscelsius(celsius):
-    return (celsius * 9 / 5) + 32 
-
-celsius = float(input("Digite a temperatura em graus celsius: "))
-
-fahrenheit = grauscelsius(celsius)
-
-print(f"{celsius} C equivalem a {fahrenheit:.2f}F")
